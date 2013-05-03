17578249b5090a29d729053492cf0692f0453d35
Arek Benedykt <arkadiusz@benedykt.net>
2013-05-03 13:10:20 +0200

    convert to mvc4 project

---
+++ CQRSGui/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace CQRSGui.App_Start
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ CQRSGui/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace CQRSGui.App_Start
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
