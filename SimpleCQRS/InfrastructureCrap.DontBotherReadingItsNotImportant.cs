529c0ba694f73c1703165cd06f6ceee576af2bb7
Payman Labbaf <payman.labbaf@just-eat.com>
2015-04-08 10:47:21 +0100

    Issue 8 - Use explicit cast instead of DelegateAdjuster.

--- SimpleCQRS/InfrastructureCrap.DontBotherReadingItsNotImportant.cs
+++ SimpleCQRS/InfrastructureCrap.DontBotherReadingItsNotImportant.cs
@@ -254,23 +254,4 @@ namespace SimpleCQRS
             return PrivateReflectionDynamicObject.WrapObjectIfNeeded(o);
         }
     }
-
-    public class DelegateAdjuster
-    {
-        public static Action<BaseT> CastArgument<BaseT, DerivedT>(Expression<Action<DerivedT>> source) where DerivedT : BaseT
-        {
-            if (typeof(DerivedT) == typeof(BaseT))
-            {
-                return (Action<BaseT>)((Delegate)source.Compile());
-
-            }
-            ParameterExpression sourceParameter = Expression.Parameter(typeof(BaseT), "source");
-            var result = Expression.Lambda<Action<BaseT>>(
-                Expression.Invoke(
-                    source,
-                    Expression.Convert(sourceParameter, typeof(DerivedT))),
-                sourceParameter);
-            return result.Compile();
-        }
-    }
 }
