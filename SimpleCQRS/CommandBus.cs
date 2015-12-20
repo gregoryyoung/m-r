57eb613c0209a460d0dc61f30cae3788791f37aa
asiraky <asiraky@gmail.com>
2015-12-21 01:36:52 +1100

    Removed bus and replaced it with a subscriber focused architecture. The changes are not significant insofar as *how* the event gets pushed to the view models, but the concept of a 'Publish(event)' has been removed (including the implementation), so as not to create the false impression that event sourcing should follow the "store event -> publish event to queue" pattern. As stated by Greg Young, using an ESB creates all sorts of headaches when wanting to rebuild view models, or add new subscribers that require events from the beginning of time, not to mention the messaging issues created when not managing the ES & ESB within a distributed transaction.
    ~TLDR: The new subscriber semantics are designed to encourage the treatment of the event store as a commit log, doing away with any notion of an ESB, where any subscriber (new or old) can for whatever reason begin receiving events from the commit log (aka the event store) at any point in time (the position in the log), be it the start of time, or regular continuations of receiving the latest events

---
+++ SimpleCQRS/CommandBus.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleCQRS
+{
+    // not to be confused with an out-of-process bus or ESB, this is just for 
+    // sending commands to command handlers using a uniform, composable method
+    public class CommandBus : ICommandSender 
+    {
+        private readonly Dictionary<Type, List<Action<Message>>> _routes = new Dictionary<Type, List<Action<Message>>>();
+
+        public void RegisterHandler<T>(Handles<T> handler) where T : Message
+        {
+            List<Action<Message>> handlers;
+
+            if (!_routes.TryGetValue(typeof(T), out handlers))
+            {
+                handlers = new List<Action<Message>>();
+                _routes.Add(typeof(T), handlers);
+            }
+
+            handlers.Add(x => handler.Handle((T)x));
+        }
+
+        public void Send<T>(T command) where T : Command
+        {
+            List<Action<Message>> handlers;
+
+            if (_routes.TryGetValue(typeof(T), out handlers))
+            {
+                if (handlers.Count != 1) throw new InvalidOperationException("cannot send to more than one handler");
+                handlers[0](command);
+            }
+            else
+            {
+                throw new InvalidOperationException("no command handler registered");
+            }
+        }
+    }
+
+    public interface Handles<in T>
+    {
+        void Handle(T message);
+    }
+
+    public interface ICommandSender
+    {
+        void Send<T>(T command) where T : Command;
+    }
+}
